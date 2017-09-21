2d5798334a7fa4ce2a1af2a92ad0edbf7c373050
Gijs Kant <gijs@thehyve.nl>
2017-09-21 10:25:07 +0200

    Add database to complete manifest.

--- manifests/complete.pp
+++ manifests/complete.pp
@@ -2,6 +2,7 @@
 class data_showcase::complete inherits data_showcase::params {
     include ::data_showcase
     include ::data_showcase::config
+    include ::data_showcase::database
     include ::data_showcase::service
 }
 
